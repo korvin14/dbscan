f573db0a1897342e7d2bfc5edf79339d6d0e8115
Debco <korvin14@gmail.com>
2016-07-19 15:52:06 +0800

    before conda sklearn check

--- hw1.py
+++ hw1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 
 
@@ -12,10 +12,6 @@ from sklearn.cluster import DBSCAN
 #     def euclidean(self, point):
 #         return (self.x - point.x)**2 + (self.y - point.y)**2
 
-def euclidian(point1, point2):
-    return (point1.x - point2.x)**2 + (point1.y - point2.y)**2
-
-
 class Cluster(object):
     def __init__(self):
         self.cl_id = 0
@@ -24,28 +20,35 @@ class Cluster(object):
         self.cl_id = cl_id
         self.cl_pts = cl_pts
 
-    def addPoint(self, p_id, p_x, p_y, p_type):
+    def addPoint(self, p_id, p_x, p_y, p_visited):
         pass
 
 
 class myDBSCAN():
-    def __init__(self, eps, mPts):
+    def __init__(self, eps, mPts, metric="euclidian"):
         self.eps = eps
         self.mPts = mPts
+        self.metric = metric
 
-    # def clusterize():
-    #     pass
+    def euclidian(point1, point2):
+        return (point1.x - point2.x)**2 + (point1.y - point2.y)**2
 
-    # def get
+    def clusterize(self, input_data):
+        pass
 
-data = pd.read_csv("order201510-small.csv",
-                   header=None, usecols=[0, 3, 4], names=['id', 'x', 'y'], dtype={'id': np.int32})
+        # def get
+
+data = pd.read_csv("order201510-small.csv", header=None,
+                   usecols=[0, 3, 4], names=['id', 'x', 'y'], dtype={'id': np.int32})
+new_data = data.assign(visited=lambda x: 0)
+
 print "head:"
-print data.head()
-print euclidian(data.iloc[1], data.iloc[1])
+print new_data.head()
+# print euclidian(data.iloc[1], data.iloc[1])
 
 
 """scikit check"""
+# print "scikit check"
 # db = DBSCAN(eps=0.001, min_samples=4).fit(data)
 # clusters = db.labels_
 # number_clusters = len(set(clusters)) - (41 if -1 in clusters else 0)
