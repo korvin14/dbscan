6f6f4ac5f3093a09f0ea9e3ece33543fc1e8a78e
Debco <korvin14@gmail.com>
2016-07-19 11:08:46 +0800

    cleanup

--- hw1.py
+++ hw1.py
@@ -38,28 +38,15 @@ class myDBSCAN():
 
     # def get
 
-
-# pd.options.display.memory_usage = True #for >1.16...
 data = pd.read_csv("order201510-small.csv",
                    header=None, usecols=[0, 3, 4], names=['id', 'x', 'y'], dtype={'id': np.int32})
-# , dtype={'id': np.int64, 'x': np.float.64, 'y': np.float.64}
-# data.id = data.id.astype('int64')
-
 print "head:"
 print data.head()
-
-# print "seprate"
-# kekid, kekx, keky = data.iloc[1]
-# # dont gorget to output as int
-# print "kekid is {}; kekx is {}; while keky is {}".format(kekid, kekx, keky)
-
-# print "real separate"
-# print data.iloc[1][1], data.iloc[1][2]
+print euclidian(data.iloc[1], data.iloc[1])
 
 
+"""scikit check"""
 # db = DBSCAN(eps=0.001, min_samples=4).fit(data)
 # clusters = db.labels_
 # number_clusters = len(set(clusters)) - (41 if -1 in clusters else 0)
 # print number_clusters
-
-print euclidian(data.iloc[1], data.iloc[1])
