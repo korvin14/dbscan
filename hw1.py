0af3bee0e27156c427ff96a9fd9a5099dc08c896
Debco <korvin14@gmail.com>
2016-07-19 17:21:21 +0800

    got dfs. added clusterzie and find neighbours

--- hw1.py
+++ hw1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 # import matplotlib.pyplot as plt
-from sklearn.cluster import DBSCAN
+# from sklearn.cluster import DBSCAN
 
 
 # class Point:
@@ -13,43 +13,81 @@ from sklearn.cluster import DBSCAN
 #         return (self.x - point.x)**2 + (self.y - point.y)**2
 
 class Cluster(object):
-    def __init__(self):
+    cluster_number = 0
+
+    def __init__(self):  # do i need this crap?
         self.cl_id = 0
 
-    def __init__(self, cl_id, cl_pts):
+    def __init__(self, cl_id, cl_pts):  # change the params
         self.cl_id = cl_id
         self.cl_pts = cl_pts
 
-    def addPoint(self, p_id, p_x, p_y, p_visited):
+    def addPoint(self, p_id, p_x, p_y, p_visited):  # change the params
         pass
 
 
 class myDBSCAN():
-    def __init__(self, eps, mPts, metric="euclidian"):
+    def __init__(self, eps, m_pts, metric="euclidian"):
         self.eps = eps
-        self.mPts = mPts
+        self.m_pts = m_pts
         self.metric = metric
+
+    def clusterize(self, input_data):
+        self.data = input_data
+        # del input_data
+        self.data_size = self.data.shape[0]
+        # for x in xrange(0, self.data_size):
+        #     if self.data.iloc[x][3] == 'y':
+        #         pass
+        #     self.data.iloc[x][3] = 'y'
+        #     neighbours = find_neighbours(self.data.iloc[x])
 
     def euclidian(point1, point2):
         return (point1.x - point2.x)**2 + (point1.y - point2.y)**2
 
-    def clusterize(self, input_data):
-        pass
-
-        # def get
+    def find_neighbours(point):
+        # neigbours = pd.DataFrame()
+        # for x in xrange(0, self.data_size):
+        pass  # some code here
+        # return neighbours
 
 data = pd.read_csv("order201510-small.csv", header=None,
                    usecols=[0, 3, 4], names=['id', 'x', 'y'], dtype={'id': np.int32})
-new_data = data.assign(visited=lambda x: 0)
+new_data = data.assign(visited=lambda x: 'n', noise=lambda x: 'n')
+
 
 print "head:"
 print new_data.head()
+print new_data.shape[0]
+print new_data.iloc[0]
+print new_data.iloc[1]
+print new_data.iloc[2]
+
+print "_____"
+print new_data.id[0]
+print new_data.x[0]
+print new_data.y[0]
+
+d1 = new_data.head(10)
+print d1
+
+df = pd.DataFrame()
+print df.empty
+df = df.append(d1.iloc[1])
+print df
+
+for x in xrange(1, 10):
+    if x % 2 == 0:
+        pass
+    else:
+        print x
 # print euclidian(data.iloc[1], data.iloc[1])
 
 
 """scikit check"""
 # print "scikit check"
+# data should be just 3 and 4th column with noheader. and that's it
 # db = DBSCAN(eps=0.001, min_samples=4).fit(data)
 # clusters = db.labels_
-# number_clusters = len(set(clusters)) - (41 if -1 in clusters else 0)
+# number_clusters = len(set(clusters)) - (1 if -1 in clusters else 0)
 # print number_clusters
