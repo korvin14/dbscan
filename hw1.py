f45be61a01d554ce10277c7e4a2099399ba4240e
Debco <korvin14@gmail.com>
2016-07-20 22:30:30 +0800

    rewritten better v, not debuged

--- hw1.py
+++ hw1.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 import pandas as pd
 
 
@@ -6,116 +6,99 @@ class myDBSCAN():
     clusters = []
     cluster_number = 0
 
+    class Cluster():
+
+        def __init__(self):
+            self.cluster_id = myDBSCAN.cluster_number
+            myDBSCAN.cluster_number += 1
+            self.cluster_points = pd.DataFrame()
+
+        def addPoint(self, point):
+            self.cluster_points = self.cluster_points.append(point)
+
     def __init__(self, eps, m_pts, metric="euclidian"):
         self.eps = eps
         self.m_pts = m_pts
         self.metric = metric  # not used in current version
 
-    class Cluster():
-
-        def __init__(self):
-            self.cl_id = myDBSCAN.cluster_number
-            myDBSCAN.cluster_number += 1
-            self.cluster_points = pd.DataFrame()
-
-        def addPoint(self, point):
-            # if : # CHECK IF WE ALREADY HAVE POINT WITH THIS ID??
-            self.cluster_points = self.cluster_points.append(point)
-            # else:
-            #     pass
-
     def clusterize(self, input_data):
         self.data = input_data
-        # del input_data  # needed??
         self.data_size = self.data.shape[0]
         for x in xrange(0, self.data_size):
-            if self.data.loc[x, 'visited'] == 'y':
-                pass  # not dummy
-            self.data.set_value(x, 'visited', 'y')
-            neighbours = self.find_neighbours(self.data.iloc[x])
-            if neighbours.shape[0] < self.m_pts:
-                self.data.set_value(x, 'noise', 'y')
-            else:
-                new_cluster = self.Cluster()
-                self.expandCluster(self.data.iloc[x], new_cluster, neighbours)
-                myDBSCAN.clusters = myDBSCAN.clusters.append(new_cluster)
-        return myDBSCAN.clusters
+            if self.data.loc[x, 'visited'] == 'n':
+                self.data.set_value(x, 'visited', 'y')
+                neighbours_id = self.find_neighbours_id(x)
+                if len(neighbours_id) < self.m_pts:
+                    self.data.set_value(x, 'noise', 'y')
+                else:
+                    new_cluster = self.Cluster()
+                    new_cluster = self.expandCluster(
+                        self.data.iloc[x], new_cluster, neighbours_id)
+                    # myDBSCAN.clusters = myDBSCAN.clusters.append(new_cluster)
+        # return myDBSCAN.clusters
 
-    def expandCluster(self, point, cluster, neighbours):
-        cluster.addPoint(point)
-        point_next = 0
-        while neighbours.iloc[point_next]:
-            if neighbours.visited[point_next] == 'n':
-                neighbours.set_value(point_next, 'visited', 'y')
-                # kostyl' and wrong - another index there is
-                self.data.visited[point_next] = 'y'
-                neighbours_next = self.find_neighbours(
-                    neighbours.iloc[point_next])
-                if neighbours_next.shape[0] >= self.m_pts:
-                    neighbours = neighbours.append(neighbours_next)
-            if self.belongToCluster(neighbours.iloc[point_next]) == 0:
-                cluster.addPoint(neighbours.iloc[point_next])
-            point_next += 1
+    def find_neighbours_id(self, point_id):
+        neighbours_id = []
+        for check_point_id in xrange(0, self.data_size):
+            if self.euclidian(self.data.iloc[point_id],
+                              self.data.iloc[check_point_id]) < self.eps:
+                neighbours_id = neighbours_id.append(check_point_id)
+        return neighbours_id
 
     def euclidian(self, point1, point2):
         return (point1[1] - point2[1])**2 + (point1[2] - point2[2])**2
 
-    def find_neighbours(self, point):
-        neighbours = pd.DataFrame()
-        for x in xrange(0, self.data_size):
-            if self.euclidian(point, self.data.iloc[x]) < self.eps:
-                neighbours = neighbours.append(self.data.iloc[x])
-        return neighbours
+    def expandCluster(self, point, cluster, neighbours_id):
+        cluster.addPoint(point)
+        point_next_id = 0
+        while True:
+            if self.data.loc[neighbours_id[point_next_id], 'visited'] == 'n':
+                self.data.set_value(
+                    neighbours_id[point_next_id], 'visited', 'y')
+                neighbours_next_id = self.find_neighbours(
+                    neighbours_id[point_next_id])
+                if len(neighbours_next_id) >= self.m_pts:
+                    neighbours_id.extend(neighbours_next_id)
+            if self.belongToCluster(neighbours_id[point_next_id]) == 0:
+                cluster.addPoint(self.data.iloc[neighbours_id[point_next_id]])
+        point_next_id += 1
+        return cluster
 
-    def belongToCluster(self, point):
+    def belongToCluster(self, point_rid):  # real id, not pd index
         belongs = 0
-        for cl in xrange(0, myDBSCAN.number_clusters):
-            for p in xrange(0, self.clusters.shape[0]):
-                if self.clusters[cl].id[p] == point.id[0]:
+        for cl in xrange(0, self.cluster_number):
+            for p in xrange(0, self.clusters[cl].shape[0]):
+                if self.clusters[cl].loc[p, 'id'] == point_rid:
                     belongs = 1
                     break
                 else:
                     pass
         return belongs
 
-    def mapClusters():
-        """takes indexes from neighbours and transform to cluster"""
-        pass
-
-pd.set_option('mode.chained_assignment', 'warn')
-data = pd.read_csv("order201510-small.csv", header=None,
-                   usecols=[0, 3, 4], names=['id', 'x', 'y'],
-                   dtype={'id': np.int32})
-upd_data = data.assign(visited=lambda x: 'n', noise=lambda x: 'n')
-# upd_data = upd_data.sort_values(['id'], ascending=[True])  # mb need sorting
-
-# db = myDBSCAN(eps=0.001, m_pts=4)
-# clusters = db.clusterize(upd_data)
-# print clusters
-
-print upd_data.head(10)
-upd_data.set_value(0, 'visited', 'y')  # updates without making new ref
-print "set value"
-print upd_data.head(10)
-
 dots = []
 for x in xrange(0, 20):
     for i in xrange(0, 20):
         if x == i:
             dots.append((x, x, i))  # tryout with np.rand(id)
-# print dots
 
-# dots2 = []
 for x in xrange(50, 70):
     for i in xrange(50, 70):
         if x == i:
             dots.append((x - 30, x, i))
-# print dots
 
 test_data = pd.DataFrame(data=dots, columns=['id', 'x', 'y'])
 test_data = test_data.assign(visited=lambda x: 'n', noise=lambda x: 'n')
 print test_data
 db = myDBSCAN(eps=1, m_pts=5)
-# clusters = db.clusterize(upd_data)
-# print clusters.cluster_number
-print test_data.loc[15, 'visited']
+clusters = db.clusterize(test_data)
+print clusters.cluster_number
+
+
+# dots = [1, 2, 465, 56786, 433412, 2323, 5]
+# shmots = [-5, 90, 9999]
+# i = 0
+# while True:
+#     print dots[i]
+#     if dots[i] == 2323:
+#         dots.extend(shmots)
+#     i += 1
